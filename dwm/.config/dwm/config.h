c7d69e5e262d3c2c4920c5b70ed62c32e6a1c472
spagnolog <gasper.spagnolo@outlook.com>
2022-05-07 23:35:39 +0200

    Changed colors a bit

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -6,18 +6,18 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "iosevka:size=11", "fontawesome:size=10" };
 static const char dmenufont[]       = "iosevka:size=11";
-static const char norm_fg[] = "#a89984";
-static const char norm_bg[] = "#282828";
-static const char norm_border[] = "#928374";
-static const char sel_fg[] = "#282828";
-static const char sel_bg[] = "#d65d0e";
-static const char sel_border[] = "#a89984";
-static const char urg_fg[] = "#a89984";
-static const char urg_bg[] = "#cc241d";
-static const char urg_border[] = "#b8bb26";
-static const char title_fg[] = "#b8bb26";
-static const char title_bg[] = "#3A3A3A";
-static const char col_borderbar[]   = "#75715e";
+
+static const char norm_fg[] = "#D5C4A1";
+static const char norm_bg[] = "#1d2021";
+static const char norm_border[] = "#665c54";
+
+static const char sel_fg[] = "#FE8019";
+static const char sel_bg[] = "#1d2021";
+static const char sel_border[] = "#fbf1c7";
+
+static const char urg_fg[] = "#fbf1c7";
+static const char urg_bg[] = "#fb4934";
+static const char urg_border[] = "#fb4934";
 
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
